0d3fa4e3158275e335aa42f2445a9cea76252f19
bnyame01 <bismark.nyame@ecomtrading.com>
2020-11-09 23:23:00 +0000

    :Covering the combining operators

--- app/src/test/java/com/bisapp/rxjavaexamples/RxJavaTest.java
+++ app/src/test/java/com/bisapp/rxjavaexamples/RxJavaTest.java
@@ -3,6 +3,8 @@ package com.bisapp.rxjavaexamples;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
@@ -13,17 +15,122 @@ import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Function;
 import io.reactivex.observers.TestObserver;
 import io.reactivex.schedulers.TestScheduler;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
 
 public class RxJavaTest {
 
     @Test
+    public void testObservableJoin() {
+
+        Observable<Long> firstObservable = Observable.interval(0, 2, TimeUnit.SECONDS).take(3);
+        Observable<Long> secondObservable = Observable.interval(6, 1, TimeUnit.SECONDS).take(3);
+        firstObservable.join(secondObservable, new Function<Long, Observable<Long>>() {
+            @Override
+            public Observable<Long> apply(Long aLong) throws Exception {
+                return Observable.timer(2, TimeUnit.SECONDS);
+            }
+        }, new Function<Long, Observable<Long>>() {
+            @Override
+            public Observable<Long> apply(Long bLong) throws Exception {
+                return Observable.timer(1, TimeUnit.SECONDS);
+            }
+        }, new BiFunction<Long, Long, String>() {
+            @Override
+            public String apply(Long aLong, Long tRight) throws Exception {
+                return aLong + " null " + tRight;
+            }
+        }).subscribe(System.out::println);
+
+    }
+
+    @Test
+    public void testObservableMerge() {
+
+        TestObserver<String> testSubscriber = new TestObserver<>();
+
+        Observable.merge(
+                Observable.fromArray(new String[]{"I", "am"}),
+                Observable.fromArray(new String[]{"trying to learn", "RxJava"})
+        ).subscribe(testSubscriber);
+
+        testSubscriber.assertValues("I", "am", "trying to learn", "RxJava");
+
+    }
+
+    @Test
+    public void testObservableStartWith() {
+
+        TestObserver<String> testSubscriber = new TestObserver<>();
+        Observable<String> first = Observable.fromArray(new String[]{"I", "am"});
+       Observable<String> second = Observable.fromArray(new String[]{"trying to learn", "RxJava"});
+
+        first.startWith(second).subscribe(testSubscriber);
+
+        testSubscriber.assertValues( "trying to learn", "RxJava","I", "am");
+
+    }
+
+    @Test
+    public void testObservableMergeWithDelayError() {
+
+        TestObserver<String> testSubscriber = new TestObserver<>();
+
+        Observable.mergeDelayError(
+                Observable.fromArray(new String[]{"hello", "world"}),
+                Observable.error(new RuntimeException("Some exception")),
+                Observable.fromArray(new String[]{"rxjava"})
+        ).subscribe(testSubscriber);
+
+        testSubscriber.assertValues("hello", "world", "rxjava");
+        testSubscriber.assertError(RuntimeException.class);
+
+    }
+
+    @Test
+    public void testObservableZip() {
+
+        TestObserver<String> testSubscriber = new TestObserver<>();
+        List<String> zippedStrings = new ArrayList<>();
+
+        Observable.zip(
+                Observable.fromArray(new String[]{"Simple", "Moderate", "Complex"}),
+                Observable.fromArray(new String[]{"Solutions", "Success", "Hierarchy"}),
+                (str1, str2) -> str1 + " " + str2).subscribe(zippedStrings::add);
+
+        Assert.assertThat(zippedStrings.size(), is(3));
+        //Assert.assertThat(zippedStrings,containsString("Simple Solutions", "Moderate Success", "Complex Hierarchy"));
+
+    }
+
+    @Test
+    public void testObservableCombineLatest() {
+
+        Observable<Integer> integerObservable = Observable.just(1, 3, 5, 6);
+        Observable<String> stringObservable = Observable.just("a", "r", "ysd");
+        Observable.combineLatest(stringObservable, integerObservable, new BiFunction<String, Integer, Object>() {
+            @Override
+            public Object apply(String s, Integer integer) throws Exception {
+                return s + integer;
+            }
+        }).withLatestFrom(stringObservable, new BiFunction<Object, String, Object>() {
+            @Override
+            public Object apply(Object o, String s) throws Exception {
+                return o + s;
+            }
+        }).subscribe(System.out::println);
+
+    }
+
+    @Test
     public void testObservableTakeLast() {
 
-        Observable.range(1,12)
+        Observable.range(1, 12)
                 .takeLast(3)
                 .subscribe(System.out::println);
 
@@ -210,7 +317,7 @@ public class RxJavaTest {
     @Test
     public void testObservableFromFuture() {
         //this creates observable from Future Task object
-        FutureTask futureTask = new FutureTask(new Callable<String>() {
+        FutureTask<String> futureTask = new FutureTask<>(new Callable<String>() {
             @Override
             public String call() throws Exception {
                 int sum = 3 + 5;
@@ -218,7 +325,7 @@ public class RxJavaTest {
             }
         });
 
-        TestObserver<String> testSubscriber = new TestObserver();
+        TestObserver<String> testSubscriber = new TestObserver<>();
         TestScheduler testScheduler = new TestScheduler();
         Observable.fromFuture(futureTask, 5, TimeUnit.SECONDS, testScheduler).doOnSubscribe(disposable -> {
             futureTask.run();
